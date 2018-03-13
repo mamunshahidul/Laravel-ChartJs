ec54dea9f4365b06080206e81b275a048ce3f538
mamunshahidul <mamunshahidul@gmail.com>
2018-03-13 13:43:01 +0600

    Added graph

--- app/Http/Controllers/DailyRevenueController.php
+++ app/Http/Controllers/DailyRevenueController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Faker\Provider\DateTime;
 use Illuminate\Support\Facades\DB;
 
 class DailyRevenueController extends Controller
@@ -10,8 +11,8 @@ class DailyRevenueController extends Controller
     public function chart()
     {
         $response = DB::table('daily_revenues')
-            ->select(DB::raw('count(*) as row_count, MONTH(created_at)'))
-            ->groupBy('MONTH(created_at)')
+            ->select(DB::raw('count(*) as row_count'))
+            ->select(DB::raw("DATE_FORMAT('created_at', '%m-%d-%Y') AS month"))
             ->get();
         return response()->json($response);
     }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,7 +33,7 @@
 
         console.log(response[0]);
         response.forEach(function (data) {
-            Months.push(data.MONTH);
+            Months.push(data.month);
         });
         var ctx = document.getElementById("myChart").getContext('2d');
         var myChart = new Chart(ctx, {
@@ -42,8 +42,8 @@
                 labels: Months,
                 datasets: [
                     {
-                        label: '# Operator',
-                        data: [12, 19, 3, 5, 2, 3],
+                        label: '# Daily Revenue',
+                        data: [12, 19, 3, 5, 2],
                         borderColor: [
                             'rgba(255,99,132,1)',
                             'rgba(54, 162, 235, 1)',
@@ -51,19 +51,6 @@
                             'rgba(75, 192, 192, 1)',
                             'rgba(153, 102, 255, 1)',
                             'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    },
-                    {
-                        label: '# CP',
-                        data: [30, 20, 11, 30, 20, 22],
-                        borderColor: [
-                            'rgba(255,99,111,1)',
-                            'rgba(54, 162, 222, 1)',
-                            'rgba(255, 206, 22, 1)',
-                            'rgba(75, 192, 222, 1)',
-                            'rgba(153, 102, 222, 1)',
-                            'rgba(255, 159, 222, 1)'
                         ],
                         borderWidth: 1
                     }
